4b65d6bc4c290803fa3bf00d67fb8c401582f000
Akshan Krithick <akshankrithick305@gmail.com>
2026-05-12 15:49:10 -0700

    initial astro + mdx + sveltia cms

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://akshan.dev',
+  integrations: [mdx(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-light',
+      wrap: true,
+    },
+  },
+});
